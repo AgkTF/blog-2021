693fea6b8717b77eab11f60783b63e07909334f4
agkz1st <ahmed.agk22@gmail.com>
2021-11-11 09:10:45 +0200

    - Add topics pages dynamically.

---
+++ pages/topics/[topicName].js
@@ -0,0 +1,56 @@
+import { PageLayout } from 'components/Layout';
+import { PostPreview } from 'components/UIElements';
+import { combinedPostsByTopic } from 'Utils/postsFetcher';
+
+const TopicPageLayout = ({ topicName, posts }) => {
+  return (
+    <PageLayout
+      pageTitle={`${topicName} Articles`}
+      pageDescription={`The homepage for the articles related to ${topicName}.`}
+      pageUrl={`https://blog.agktf.com/topics/${topicName}`}
+    >
+      <main className="pb-16 mt-5 max-w-sm sm:max-w-xl md:max-w-2xl lg:max-w-3xl m-auto">
+        <section className="mt-6 sm:mt-10 w-full">
+          <h1 className="font-bold font-sansita text-headline-light dark:text-headline-dark text-2xl sm:text-3xl">
+            {topicName}
+          </h1>
+
+          {combinedPostsByTopic[topicName].map(post => {
+            const { postTitle, publishDate, tagName, postPreview } =
+              post.module.meta;
+            return (
+              <PostPreview
+                key={post.link}
+                contentPreview={postPreview}
+                tagName={tagName}
+                date={publishDate}
+                postTitle={postTitle}
+                link={post.link}
+              />
+            );
+          })}
+        </section>
+      </main>
+    </PageLayout>
+  );
+};
+
+export async function getStaticPaths() {
+  const paths = Object.keys(combinedPostsByTopic).map(topic => ({
+    params: {
+      topicName: topic,
+    },
+  }));
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+  return {
+    props: {
+      topicName: params.topicName,
+    },
+  };
+}
+
+export default TopicPageLayout;
--- pages/topics/react.js
+++
@@ -1,7 +0,0 @@
-import { TopicPageLayout } from 'components/Layout';
-
-const ReactPosts = () => {
-  return <TopicPageLayout topicName="React" />;
-};
-
-export default ReactPosts;
