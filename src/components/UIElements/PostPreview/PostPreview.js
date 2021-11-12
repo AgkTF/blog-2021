1ea98878315f3759ca3e9c14559b18a5d8149696
agkz1st <ahmed.agk22@gmail.com>
2021-11-12 21:44:08 +0200

    Move the tags next to read more in "PostPreview.js".

--- src/components/UIElements/PostPreview/PostPreview.js
+++ src/components/UIElements/PostPreview/PostPreview.js
@@ -10,27 +10,33 @@ const PostPreview = ({ tagName, date, postTitle, contentPreview, link }) => {
   return (
     <>
       <div className="mt-6 flex flex-col">
-        <div className="flex justify-between items-baseline">
-          <TagPill tagName={tagName} />
-          <p className="text-xs text-paragraph-light dark:text-paragraph-dark font-rubik sm:text-sm">
-            {enUSDateRenderer(date)}
-          </p>
-        </div>
-
         <div className="mt-2">
-          <Title postTitle={postTitle} link={link} className="" />
+          <div className="flex justify-between items-center">
+            <Title postTitle={postTitle} link={link} />
+            <p className="text-xs text-paragraph-light dark:text-paragraph-dark font-rubik sm:text-sm">
+              {enUSDateRenderer(date)}
+            </p>
+          </div>
 
           <p className="mt-2 font-rubik text-paragraph-light dark:text-paragraph-dark text-sm sm:text-base">
             {contentPreview}
           </p>
         </div>
 
-        <Link href={link}>
-          <a className="mt-2 flex items-center font-rubik text-tertiary-light dark:text-tertiary-dark hover:text-highlight dark:hover:text-highlight self-end">
-            <span className="font-medium text-sm sm:text-base">Read more</span>
-            <ArrowRightIcon className="ml-1 w-4 h-4" />
-          </a>
-        </Link>
+        <div className="mt-4 flex justify-between items-center">
+          <div className="flex items-center justify-start gap-2">
+            <TagPill tagName={tagName} />
+          </div>
+
+          <Link href={link}>
+            <a className="mt-2 flex items-center font-rubik text-tertiary-light dark:text-tertiary-dark hover:text-highlight dark:hover:text-highlight self-end">
+              <span className="font-medium text-sm sm:text-base">
+                Read more
+              </span>
+              <ArrowRightIcon className="ml-1 w-4 h-4" />
+            </a>
+          </Link>
+        </div>
       </div>
 
       <hr className="mt-4 opacity-10 text-paragraph-light dark:text-paragraph-dark" />
