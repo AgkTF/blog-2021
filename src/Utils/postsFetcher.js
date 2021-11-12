962e85a0553f9cf882d1df3f3af62a4c38f152a8
agkz1st <ahmed.agk22@gmail.com>
2021-11-12 21:15:54 +0200

    - Move all the posts outside the blog folder, I thought it's redundant to add /blog before the post title when the base url itself is blog.agktf.com.

--- src/Utils/postsFetcher.js
+++ src/Utils/postsFetcher.js
@@ -1,12 +1,12 @@
 function importAll(r) {
   return r.keys().map(filename => {
     return {
-      link: `/blog${filename.substr(1).replace(/\/index\.mdx$/, '')}`,
+      link: `${filename.substr(1).replace(/\/index\.mdx$/, '')}`,
       module: r(filename),
     };
   });
 }
 
 export const allPosts = importAll(
-  require.context('../../pages/blog', true, /\.mdx$/)
+  require.context('../../pages', true, /\.mdx$/)
 );
